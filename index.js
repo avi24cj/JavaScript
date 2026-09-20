af597ff7b0087e44108e03269c1ed3e97fb3f7b5
Avisek <das.avisek@gmail.com>
2026-09-19 20:06:50 -0500

    First JS commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let msg = 'Welcome to JS learning ...';
+let age = 40;
+const accountNumber =12345
+console.log(msg , "age is ---> ",age);
+console.log("account number is ---> ",accountNumber);
+age =30;
+//accountNumber = 4321; // constant
+console.log(msg , "age is ---> ",age);
+console.log("account number is ---> ",accountNumber);
