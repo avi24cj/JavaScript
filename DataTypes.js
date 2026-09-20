db91943738af47d6aa598807225dc35f5b41014d
Avisek <das.avisek@gmail.com>
2026-09-20 00:39:56 -0500

    data type poc

---
+++ DataTypes.js
@@ -0,0 +1,17 @@
+let my_name = "Avi";
+let age = 39.5
+let height = "5.5ft"
+const dob = "20-02-2015"
+let city = null
+let country
+let transferAmount = 100_00_000
+console.log(typeof my_name);
+console.log(typeof age);
+console.log(typeof height);
+console.log(typeof dob);
+console.log(typeof city);
+console.log(typeof country);
+console.log(transferAmount)
+console.log(Number.MAX_VALUE*10)
+let bigIntDemo =3456565577687878787878n
+console.log(bigIntDemo, typeof bigIntDemo)
