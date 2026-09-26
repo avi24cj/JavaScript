ebefb7cc80ddaedb4cc06e9f4cefcb4e4f5cc30d
Avisek <das.avisek@gmail.com>
2026-09-26 11:22:29 -0500

    Operator

---
+++ Operators.js
@@ -0,0 +1,7 @@
+let num = 5;
+let num_preincrement = num++;
+console.log(num, num_preincrement);
+let num_postincrement = num++;
+console.log(num, num_postincrement);
+num = num**2
+console.log("Pow of 2 example",num)
